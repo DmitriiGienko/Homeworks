37d6c1e8047f7e7bbb8307330b900506caffc585
Dmitrii Gienko <cuanik@mail.ru>
2023-01-09 22:24:52 +0300

    продолжаю делать домашку

--- src/pro/sky/java/course1/lesson13/Homework13.java
+++ src/pro/sky/java/course1/lesson13/Homework13.java
@@ -1,4 +1,6 @@
-public class Main {
+package pro.sky.java.course1.lesson13;
+
+public class Homework13 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
